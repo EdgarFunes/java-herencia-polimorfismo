0803946e3868bdf20e742b7c7570f7c5c558ee0b
EdgarFunes <edgardoibarfunesortiz@gmail.com>
2024-11-27 11:36:34 -0300

    Create VueloCharter.java

---
+++ VueloCharter.java
@@ -0,0 +1,23 @@
+class VueloCharter extends Vuelo implements Promocionable {
+    private double precioTotal;
+
+    public VueloCharter(int numeroVuelo, String origen, String destino, String fecha, double precioTotal) {
+        super(numeroVuelo, origen, destino, fecha);
+        this.precioTotal = precioTotal;
+    }
+
+    @Override
+    public double calcularPrecio() {
+        return precioTotal;
+    }
+
+    @Override
+    public void aplicarPromocion(double descuento) {
+        this.precioTotal -= this.precioTotal * descuento / 100;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Tipo: Charter | Precio Total: " + calcularPrecio();
+    }
+}
